d6a7fda4f7502e99a0d241a2c36b3767fd502b91
Leo <39671641+LeopoldHock@users.noreply.github.com>
2020-10-06 12:04:34 +0200

    Generalization
    Updated the code to make it more general and instructive

--- camera_photo.js
+++ camera_photo.js
@@ -1,68 +1,45 @@
- startWebcam: function () {
-                var that = this;
-                if (navigator.getUserMedia) {
-                    navigator.getUserMedia(
+// Library available here: https://github.com/mebjas/html5-qrcode
+// Copy HTML5Qrcode.js into "libs" directory in SAPUI5 root directory.
+  qrScannerRequest: function () {
+		var that = this;
+		var containerId = this.getView().byId("qrContainer").sId; // qrContainer is just a simple <div>: <html:div id="qrContainer"></html:div>
+		if (containerId) {
+			qrCodeReader = new Html5Qrcode(containerId);
+			// This method will trigger user permission request
+			Html5Qrcode.getCameras().then(function (devices) {
+				if (devices && devices.length) {
+					var cameraId = devices[0].id;
+					that.qrScannerStart(cameraId);
+				}
+			}).catch(function (error) {
+				console.error(error);
+			});
+        } else {
+			console.error("Unable to find QR container ID.");
+		}
+	},
 
- 
-
-                        // constraints
-                        {
-                            video: true,
-                            audio: false
-                        },
-                        // successCallback
-                        function (localMediaStream) {
-                            video = document.querySelector("video");
-                            var blob = new Blob([video]);
-                            var url = URL.createObjectURL(blob);
-                            //video.src = window.URL.createObjectURL(localMediaStream);
-                            //URL.revokeObjectURL(url);
-                            webcamStream = localMediaStream;
-                            var mediaStreamTrack = webcamStream.getVideoTracks()[0];
-                            var imageCapture = new ImageCapture(mediaStreamTrack);
-                            that.takePhoto(imageCapture);
-                        },
-                        // errorCallback
-                        function (error) {
-                            console.log("The following error occured: " + error);
-                        }
-                    );
-                } else {
-                    console.log("getUserMedia not supported");
-                }
-            },
-
- 
-
-            takePhoto: function (imageCapture) {
-                var that = this;
-                //    var img = imageCapture || document.querySelector("img");
-                imageCapture.takePhoto().then(function (blob) {
-                    var img = that.getView().byId("camImage");
-                    img.setActiveSrc(window.URL.createObjectURL(blob));
-                    //img.classList.remove("hidden");
-                    //img.src = URL.createObjectURL(blob);
-                    console.log("Took photo:", window.URL.createObjectURL(blob));
-                    console.log(img.getActiveSrc());
-                }).catch(function (error) {
-                    console.log("takePhoto() error: ", error);
-                });
-                /*
-                imageCapture.takePhoto(
-                    function (blob) {
-                        var url = window.URL.createObjectURL(blob);
-                        img.src = url;
-                        window.URL.revokeObjectURL(url);
-                    },
-                    function (error) {
-                        console.log("The following error occured: " + error);
-                    });
-                
-                .then(blob => {
-                    let url = window.URL.createObjectURL(blob);
-                    img.src = url;
-
- 
-
-                    window.URL.revokeObjectURL(url);
-                })*/
+    qrScannerStart: function (cameraId) {
+		var that = this;
+		qrCodeReader.start(
+			cameraId, {
+				fps: 10, // Optional frame per seconds for qr code scanning
+				qrbox: 250 // Optional if you want bounded box UI
+			},
+			function (result) {
+				driverData.Url = result;
+				that.qrScannerStop(undefined, result);
+			}).catch(function (error) {
+			// do nothing
+		});
+	},
+        
+    qrScannerStop: function (event, result) {
+		var that = this;
+		qrCodeReader.stop().then(function (ignore) {
+			// QR Code scanning is stopped.
+		}).catch(function (error) {
+			console.error(error);
+			// Stop failed, handle it.
+		});
+	}
